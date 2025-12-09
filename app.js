d7d9fee1e5c63d45bd3e3be18dc21761b33577a9
Jhiovana Silva <jhiovanasilva11@gmail.com>
2025-12-09 19:50:49 -0300

    Refactor app.js: remove unused JWT_SECRET variable and clean up route imports

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 require('dotenv').config();
 const express = require('express');
-const jwt = require('jsonwebtoken');
-
 const app = express();
 
 // Servidor
 const PORT = process.env.PORT || 3000;
-const JWT = process.env.JWT_SECRET;
+
+// Importar Módulos de Rotas 
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const publicRoutes = require('./routes/publicRoutes');
+
+app.use(express.json()); 
+
+// ROTAS PÚBLICAS: Lida com /login e /cadastro
+app.use('/', publicRoutes); 
+
+// ROTAS PROTEGIDAS: Lida com /api/usuarios, etc.
+app.use('/api/usuarios', usuarioRoutes);
 
 // Rota Principal 
 app.get('/', (req, res) => {
     res.send("Bem vindo a API do sistema Drive Easy").status(200);
 });
-
-//Usuarios e perfis
 
 
 app.listen(PORT, () => {
