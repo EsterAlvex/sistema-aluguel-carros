6d8b137d2256707127acc5d152faaf88bb131d75
Isabela <isabelamarques046@gmail.com>
2025-12-11 17:17:51 -0300

    configuração do jsonwebtoken

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+const JWT = process.env.JWT_SECRET;
 
 //Rota Principal
 app.get('/', (req, res) => {
-    res.send("Bem vindo a Loja EasyCar").status(200);
+    res.send("Bem vindo a API do sistema Drive Easy").status(200);
 });
 
 //Usuarios e perfis
