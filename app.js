a95942d54a14e4352667e0f911a2e9915fa9859b
jhsribeiro <jhiovanasilva11@gmail.com>
2025-12-12 10:58:21 -0300

    apenas organizando o código

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
 const app = express();
+const JWT = process.env.JWT_SECRET;
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const JWT = process.env.JWT_SECRET;
 
 const { Usuario } = require('./models');
 const { Carro } = require('./models');
