324ac460d9110dfda9ca2d8b219a3b72bea98269
isabelamarquess <isabelamarques046@gmail.com>
2025-12-15 20:32:16 -0300

    removi eCliente middleware de autorizacao.js

--- middleware/autorizacao.js
+++ middleware/autorizacao.js
@@ -5,12 +5,5 @@ const eFuncionario = (req, res, next) => {
     next();
 };
 
-const eCliente = (req, res, next) => {
-    
-    if (!req.usuario || req.usuario.tipo !== 'Cliente') {
-        return res.status(403).json({ mensagem: "Acesso negado. Ação exclusiva para Clientes." });
-    }
-    next();
-};
 
-module.exports = { eFuncionario, eCliente };
+module.exports = { eFuncionario };
